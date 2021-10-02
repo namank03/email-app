a82b222c18849201cfab0f9c9224ae2a3a2e8f43
namank03 <khandelwalnaman03@gmail.com>
2021-10-02 20:16:01 +0530

    Added email-show-css-part-1
    Co-authored-by: anushkatiwari455 <anushkatiwari455@users.noreply.github.com>

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Email } from "../services/email.service";
 
 @Component({
-  selector: 'app-email-show',
-  templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+	selector: "app-email-show",
+	templateUrl: "./email-show.component.html",
+	styleUrls: ["./email-show.component.css"]
 })
 export class EmailShowComponent implements OnInit {
+	@Input() email!: Email | null;
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/inbox/services/email.service.ts
+++ src/app/inbox/services/email.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-interface EmailSummary {
+export interface EmailSummary {
 	from: string;
 	id: string;
 	subject: string;
 }
 
-interface Email {
+export interface Email {
 	from: string;
 	html: string;
 	id: string;
